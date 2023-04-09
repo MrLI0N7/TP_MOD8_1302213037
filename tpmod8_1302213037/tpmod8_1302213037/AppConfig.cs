c583287ddfd57a059d79b3943c864c479313132e
MrLI0N7 <93116969+MrLI0N7@users.noreply.github.com>
2023-04-09 18:50:05 +0800

    Menyelesaikan program dengan runtime configuration

---
+++ tpmod8_1302213037/tpmod8_1302213037/AppConfig.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace tpmod8_1302213037
+{
+    internal class AppConfig
+    {
+        public CovidConfig covidConfig;
+        public void readConfig()
+        {
+            string hasilbaca = File.ReadAllText(@"./covid_config.json");
+            covidConfig = JsonSerializer.Deserialize<CovidConfig>(hasilbaca);
+        }
+        public void writeConfig()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+
+            String jsonString = JsonSerializer.Serialize(covidConfig, options);
+
+            File.WriteAllText(@"./covid_config.json", jsonString);
+        }
+        public void defaultConfig()
+        {
+            covidConfig = new CovidConfig("celcius", 14, "Anda tidak diperbolehkan masuk ke dalam gedung ini", "Anda dipersilahkan untuk masuk gedung ini");
+        }
+        public void UbahSatuan()
+        {
+            if (covidConfig.satuan_suhu == "celcius") 
+            {
+                covidConfig.satuan_suhu = "fahreinheit";
+                writeConfig();
+            } else {
+                covidConfig.satuan_suhu = "celcius";
+                writeConfig();
+            }
+        }
+    }
+}
--- tpmod8_1302213037/tpmod8_1302213037/Program.cs
+++ tpmod8_1302213037/tpmod8_1302213037/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tpmod8_1302213037;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        AppConfig config1 = new AppConfig();
+        try
+        {
+            config1.readConfig();
+        }
+        catch
+        {
+            config1.defaultConfig();
+            config1.writeConfig();
+            config1.readConfig();
+        }
+        config1.UbahSatuan();
+
+        Console.WriteLine("Berapa suhu badan anda pada saat ini? Dalam nilai "+ config1.covidConfig.satuan_suhu);
+        double inputSuhu = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Berapa hari yang lalu(perkiraan) anda terakhir memiliki gejala demam ?");
+        int inputHari = Convert.ToInt32(Console.ReadLine());
+
+        if((config1.covidConfig.satuan_suhu == "celcius" &&(inputSuhu >= 36.5 && inputSuhu < 37.5)) || (config1.covidConfig.satuan_suhu == "fahreinheit" && (inputSuhu >= 97.7 && inputSuhu < 99.5))) 
+        {
+            Console.WriteLine(config1.covidConfig.pesan_diterima);
+        } 
+        else if (inputHari > config1.covidConfig.batas_hari_demam)
+        {
+            Console.WriteLine(config1.covidConfig.pesan_diterima);
+        }
+        else
+        {
+            Console.WriteLine(config1.covidConfig.pesan_ditolak);
+        }
+    }
+}
